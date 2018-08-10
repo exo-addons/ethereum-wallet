bd6c96ea43efa2068dd7006229b3788b3e31f396
Boubaker Khanfir <bkhanfir@exoplatform.com>
2018-08-10 10:24:45 +0100

    Add title to general transactions to display the latest blocks count used

--- exo-ethereum-wallet-webapps/src/main/webapp/vue-app/components/GeneralTransactions.vue
+++ exo-ethereum-wallet-webapps/src/main/webapp/vue-app/components/GeneralTransactions.vue
@@ -1,6 +1,7 @@
 <template>
   <v-flex v-if="transactions.length">
     <v-card class="card--flex-toolbar">
+      <v-subheader>Transactions list retrieved from latest {{ maxBlocksToLoad }} blocks</v-subheader>
       <v-list two-line class="pt-0 pb-0">
         <template v-for="(item, index) in sortedTransaction">
           <v-list-tile :key="item.hash" avatar>
