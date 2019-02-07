cb0844a471c9659c9c147a1682d05114b2805471
exo <nermineboujelben@gmail.com>
2019-02-07 11:56:08 +0100

    add wallet Request Funds Modal, Wallet settings Modal and Wallet Summary tests

---
+++ exo-ethereum-wallet-webapps/src/test/specs/WalletRequestFundsModal.test.js
@@ -0,0 +1,126 @@
+import {getWalletApp, initApp, getTransactions, expectCountElement, expectObjectValueEqual, initiateBrowserWallet, sendTokens, sendEther} from '../TestUtils.js';
+
+import WalletRequestFundsModal from '../../main/webapp/vue-app/components/WalletRequestFundsModal';
+
+import {mount} from '@vue/test-utils';
+import {hashCode} from '../../main/webapp/vue-app/WalletUtils.js';
+
+import flushPromises from 'flush-promises';
+
+jest.setTimeout(30000);
+
+describe('WalletRequestFundsModal.test.js', () => {
+  const app = getWalletApp();
+
+  beforeAll(() => {
+    return initApp(app);
+  });
+
+  const defaultAttributesValues = {
+    icon: false,
+    walletAddress: false,
+    accountsDetails: {},
+    overviewAccounts: [],
+    principalAccount: false,
+    refreshIndex: 0,
+    selectedOption: false,
+    recipient: false,
+    amount: false,
+    error: false,
+    requestMessage: '',
+    loading: false,
+    dialog: false,
+  };
+
+  it('WalletRequestFundsModal default data', () => {
+    console.log('-- Test WalletRequestFundsModal default data');
+
+    const walletRequestFundsModal = mount(WalletRequestFundsModal, {
+      attachToDocument: true,
+    });
+
+    expectObjectValueEqual(walletRequestFundsModal.vm, defaultAttributesValues, 'WalletRequestFundsModal default data: ');
+  });
+
+  it('WalletRequestFundsModal - test WalletRequestFundsModal enabled', (done) => {
+    console.log('--- WalletRequestFundsModal - test WalletRequestFundsModal enabled');
+
+    global.walletAddress = global.walletAddresses[0];
+    global.defaultWalletSettings.defaultPrincipalAccount = global.tokenAddress;
+    global.defaultWalletSettings.defaultOverviewAccounts = global.defaultWalletSettings.defaultContractsToDisplay = [global.tokenAddress, 'ether'];
+
+    const app = getWalletApp();
+    let contractDetails, walletSummary, walletRequestFundsModal;
+    return initApp(app)
+      .then(() => {
+        contractDetails = app.vm.accountsDetails[global.tokenAddress];
+        expect(contractDetails).toBeTruthy();
+        app.vm.openAccountDetail(contractDetails);
+        return app.vm.$nextTick();
+      })
+      .then(() => {
+        walletSummary = app.vm.$refs.walletSummary;
+        expect(walletSummary).toBeTruthy();
+      })
+
+      .then(() => {
+        walletRequestFundsModal = walletSummary.$refs.walletRequestFundsModal;
+        expect(walletRequestFundsModal).toBeTruthy();
+        expect(walletRequestFundsModal.error).toBeNull();
+      })
+
+      .then(() => {
+        walletRequestFundsModal.requestFunds();
+        expect(walletRequestFundsModal.error).not.toBeNull();
+      })
+
+      .then(() => done())
+      .catch((e) => done(e));
+  });
+
+  it('WalletRequestFundsModal - test send Request', (done) => {
+    console.log('--- WalletRequestFundsModal - test send Request');
+
+    global.walletAddress = global.walletAddresses[0];
+    global.defaultWalletSettings.defaultPrincipalAccount = global.tokenAddress;
+    global.defaultWalletSettings.defaultOverviewAccounts = global.defaultWalletSettings.defaultContractsToDisplay = [global.tokenAddress, 'ether'];
+
+    const app = getWalletApp();
+    let contractDetails, walletSummary, walletRequestFundsModal;
+    return initApp(app)
+      .then(() => {
+        contractDetails = app.vm.accountsDetails[global.walletAddress];
+        expect(contractDetails).toBeTruthy();
+        app.vm.openAccountDetail(contractDetails);
+        return app.vm.$nextTick();
+      })
+      .then(() => {
+        walletSummary = app.vm.$refs.walletSummary;
+        expect(walletSummary).toBeTruthy();
+      })
+
+      .then(() => {
+        walletRequestFundsModal = walletSummary.$refs.walletRequestFundsModal;
+        expect(walletRequestFundsModal).toBeTruthy();
+        walletRequestFundsModal.dialog = true;
+        expect(walletRequestFundsModal.error).toBeNull();
+      })
+
+      .then(() => {
+        walletRequestFundsModal.recipient = global.walletAddresses[5];
+        walletRequestFundsModal.amount = 4;
+        walletRequestFundsModal.requestFunds();
+        expect(walletRequestFundsModal.error).toBeNull();
+      })
+
+      .then(() => {
+        walletRequestFundsModal.recipient = global.walletAddresses[4444444];
+        walletRequestFundsModal.requestFunds();
+        expect(walletRequestFundsModal.error).not.toBeNull();
+        return flushPromises();
+      })
+
+      .then(() => done())
+      .catch((e) => done(e));
+  });
+});
---
+++ exo-ethereum-wallet-webapps/src/test/specs/WalletSummary.test.js
@@ -0,0 +1,121 @@
+import {getWalletApp, initApp, getTransactions, expectCountElement, expectObjectValueEqual, initiateBrowserWallet, sendTokens, sendEther} from '../TestUtils.js';
+
+import WalletSummary from '../../main/webapp/vue-app/components/WalletSummary';
+
+import {mount} from '@vue/test-utils';
+import {hashCode} from '../../main/webapp/vue-app/WalletUtils.js';
+
+import flushPromises from 'flush-promises';
+
+jest.setTimeout(30000);
+
+describe('WalletSummary.test.js', () => {
+  const app = getWalletApp();
+
+  beforeAll(() => {
+    return initApp(app);
+  });
+
+  const defaultAttributesValues = {
+    accountsDetails: {},
+    networkId: 0,
+    walletAddress: false,
+    isMaximized: false,
+    isSpace: false,
+    isSpaceAdministrator: false,
+    hideActions: false,
+    overviewAccounts: [],
+    principalAccount: false,
+    refreshIndex: 0,
+    etherBalance: 0,
+    totalBalance: 0,
+    totalFiatBalance: 0,
+    fiatSymbol: '$',
+    isReadOnly: false,
+    updatePendingTransactionsIndex: 1,
+    pendingTransactions: {},
+    disableSendButton: true,
+    pendingTransactionsCount: false,
+    principalAccountDetails: false,
+    overviewAccountsArray: [],
+  };
+
+  it('WalletSummary default data', () => {
+    console.log('-- Test WalletSummary default data');
+
+    const walletSummary = mount(WalletSummary, {
+      attachToDocument: true,
+    });
+
+    expectObjectValueEqual(walletSummary.vm, defaultAttributesValues, 'walletSummary default data: ');
+  });
+
+  it('WalletSummary - wallet summary is enabled', (done) => {
+    console.log('--- test wallet summary is enabled');
+
+    global.walletAddress = global.walletAddresses[0];
+    global.defaultWalletSettings.defaultPrincipalAccount = global.tokenAddress;
+    global.defaultWalletSettings.defaultOverviewAccounts = global.defaultWalletSettings.defaultContractsToDisplay = [global.tokenAddress, 'ether'];
+
+    const app = getWalletApp();
+
+    return initiateBrowserWallet(global.walletAddress, 'testpassword', /* Not space*/ false, /* generated */ true, /* not backedup */ false)
+      .then(() => initApp(app))
+      .then(() => app.vm.$nextTick())
+      .then(() => {
+        const accountDetails = app.vm.accountsDetails[global.tokenAddress];
+        expect(accountDetails).not.toBeNull();
+        app.vm.openAccountDetail(accountDetails);
+        return app.vm.$nextTick();
+      })
+      .then(() => {
+        expectCountElement(app, 'waletSummary', 1);
+
+        const walletSummary = app.vm.$refs.walletSummary;
+        expect(walletSummary).toBeTruthy();
+      })
+      .then(() => done())
+      .catch((e) => done(e));
+  });
+
+  it('WalletSummary --test refresh Balance  of default contract event', () => {
+    console.log('-- WalletSummary --test refresh Balance  of default contract event');
+    const walletSummary = mount(WalletSummary, {
+      attachToDocument: true,
+    });
+    expect(walletSummary.emitted()['refresh-balance']).toBeFalsy();
+    walletSummary.vm.refreshBalance(global.defaultWalletSettings.defaultContractsToDisplay);
+    expect(walletSummary.emitted()['refresh-balance']).toBeTruthy();
+  });
+
+  it('WalletSummary - test refresh Balance  of token contract event', (done) => {
+    console.log('--- test refresh Balance  of token contract event');
+
+    global.walletAddress = global.walletAddresses[0];
+    global.defaultWalletSettings.defaultPrincipalAccount = global.tokenAddress;
+    global.defaultWalletSettings.defaultOverviewAccounts = global.defaultWalletSettings.defaultContractsToDisplay = [global.tokenAddress, 'ether'];
+
+    const app = getWalletApp();
+    let walletSummary, accountDetails;
+    return initiateBrowserWallet(global.walletAddress, 'testpassword', /* Not space*/ false, /* generated */ true, /* not backedup */ false)
+      .then(() => initApp(app))
+      .then(() => app.vm.$nextTick())
+      .then(() => {
+        accountDetails = app.vm.accountsDetails[global.tokenAddress];
+        expect(accountDetails).not.toBeNull();
+        app.vm.openAccountDetail(accountDetails);
+        return app.vm.$nextTick();
+      })
+      .then(() => {
+        const walletSummary = mount(WalletSummary, {
+          attachToDocument: true,
+        });
+        expect(walletSummary.emitted()['refresh-token-balance']).toBeFalsy();
+        walletSummary.vm.refreshBalance(accountDetails);
+        expect(walletSummary.emitted()['refresh-token-balance']).toBeTruthy();
+      })
+
+      .then(() => done())
+      .catch((e) => done(e));
+  });
+});
