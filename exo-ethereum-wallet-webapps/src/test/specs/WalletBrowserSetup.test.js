85917b1e80b56d55e53efc655bd427c459538920
exo <nermineboujelben@gmail.com>
2019-02-07 11:48:37 +0100

    add wallet Backup Modal, wallet Browser Setup and wallet Metamask Setup tests

---
+++ exo-ethereum-wallet-webapps/src/test/specs/WalletBrowserSetup.test.js
@@ -0,0 +1,75 @@
+import {getWalletApp, initApp, getTransactions, expectCountElement, expectObjectValueEqual, initiateBrowserWallet, sendTokens, sendEther} from '../TestUtils.js';
+
+import WalletBrowserSetup from '../../main/webapp/vue-app/components/WalletBrowserSetup';
+
+import {mount} from '@vue/test-utils';
+import {hashCode} from '../../main/webapp/vue-app/WalletUtils.js';
+
+import flushPromises from 'flush-promises';
+
+jest.setTimeout(30000);
+
+describe('WalletBrowserSetup.test.js', () => {
+  const app = getWalletApp();
+
+  beforeAll(() => {
+    return initApp(app);
+  });
+
+  const defaultAttributesValues = {
+    isSpace: false,
+    isSpaceAdministrator: false,
+    isAdministration: false,
+    loading: false,
+    refreshIndex: 0,
+    walletAddress: global.walletAddresses[1],
+    loadingWalletBrowser: false,
+  };
+
+  it('WalletBrowserSetup default data', () => {
+    console.log('-- Test WalletBrowserSetup default data');
+
+    const walletBrowserSetup = mount(WalletBrowserSetup, {
+      attachToDocument: true,
+    });
+
+    expectObjectValueEqual(walletBrowserSetup.vm, defaultAttributesValues, 'walletBrowserSetup default data: ');
+  });
+
+  it('WalletBrowserSetup test when creating new wallet Address', (done) => {
+    console.log('-- Test WalletBrowserSetup test when creating new wallet Address');
+
+    let walletSetup, walletBrowserSetup;
+    return initApp(app)
+      .then(() => app.vm.$nextTick())
+      .then(() => {
+        walletSetup = app.vm.$refs.walletSetup;
+        expect(walletSetup).toBeTruthy();
+
+        walletSetup.displayWalletSetup = true;
+
+        walletBrowserSetup = walletSetup.$refs.walletBrowserSetup;
+        expect(walletBrowserSetup).toBeTruthy();
+
+        walletBrowserSetup.createWallet();
+
+        expect(walletBrowserSetup.loadingWalletBrowser).toBeTruthy();
+
+        return flushPromises();
+      })
+
+      .then(() => done())
+      .catch((e) => done(e));
+  });
+
+  it('WalletBrowserSetup test when switchToMetamask event', () => {
+    console.log('-- WalletBrowserSetup test when switchToMetamask event');
+
+    const walletBrowserSetup = mount(WalletBrowserSetup, {
+      attachToDocument: true,
+    });
+
+    walletBrowserSetup.vm.switchToMetamask();
+    expect(walletBrowserSetup.emitted()['configured']).toBeTruthy();
+  });
+});
---
+++ exo-ethereum-wallet-webapps/src/test/specs/WalletMetamaskSetup.test.js
@@ -0,0 +1,66 @@
+import {getWalletApp, initApp, getTransactions, expectCountElement, expectObjectValueEqual, initiateBrowserWallet, sendTokens, sendEther} from '../TestUtils.js';
+
+import WalletMetamaskSetup from '../../main/webapp/vue-app/components/WalletMetamaskSetup.vue';
+
+import {mount} from '@vue/test-utils';
+
+import flushPromises from 'flush-promises';
+
+jest.setTimeout(30000);
+
+describe('WalletMetamaskSetup.test.js', () => {
+  const app = getWalletApp();
+
+  beforeAll(() => {
+    return initApp(app);
+  });
+
+  const defaultAttributesValues = {
+    isSpace: false,
+    isSpaceAdministrator: false,
+    loading: false,
+    isAdministration: false,
+    walletAddress: false,
+    refreshIndex: 0,
+    addressAssociationDialog: false,
+    installInstructionDialog: false,
+    networkLabel: false,
+    principalContractAdminAddress: global.walletAddresses[0],
+    sameConfiguredNetwork: true,
+    associatedWalletAddress: global.walletAddress,
+    detectedMetamaskAccount: false,
+    currentAccountAlreadyInUse: false,
+    metamaskEnabled: false,
+    metamaskConnected: false,
+    displayNotSameNetworkWarning: false,
+    isPrincipalContractAdmin: false,
+    displaySpaceAccountAssociationHelp: false,
+    displayUserAccountAssociationHelp: false,
+    displayAddressAssociationBox: false,
+    displayUserAccountChangeHelp: false,
+    displayAccountHelpActions: false,
+    displaySpaceMetamaskEnableHelp: false,
+    newAddressDetected: false,
+  };
+
+  it('WalletMetamaskSetup default data', () => {
+    console.log('-- WalletMetamaskSetup default data');
+
+    const walletMetamaskSetup = mount(WalletMetamaskSetup, {
+      attachToDocument: true,
+    });
+
+    expectObjectValueEqual(walletMetamaskSetup.vm, defaultAttributesValues, 'walletMetamaskSetup default data');
+  });
+
+  it('WalletMetamaskSetup test when disableMetasmaskUsage event', () => {
+    console.log('-- WalletMetamaskSetup test when disableMetasmaskUsage event');
+
+    const walletMetamaskSetup = mount(WalletMetamaskSetup, {
+      attachToDocument: true,
+    });
+
+    walletMetamaskSetup.vm.disableMetamaskUsage();
+    expect(walletMetamaskSetup.emitted()['refresh']).toBeTruthy();
+  });
+});
