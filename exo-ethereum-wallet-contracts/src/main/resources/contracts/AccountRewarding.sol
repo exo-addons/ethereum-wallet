b4efadfd8e33cf66849c926b10860ceb0949f4b9
Boubaker Khanfir <bkhanfir@exoplatform.com>
2019-04-08 11:48:52 +0100

    Enhance reward operation to make it more flexible

--- exo-ethereum-wallet-contracts/src/main/resources/contracts/AccountRewarding.sol
+++ exo-ethereum-wallet-contracts/src/main/resources/contracts/AccountRewarding.sol
@@ -16,22 +16,21 @@ contract AccountRewarding is ERTTokenV1 {
     /**
      * @dev reward `_value` token to `_to` from `msg.sender`
      * @param _to The address of the recipient
-     * @param _value The amount of token to be rewarded
+     * @param _amount The amount of token to be transferred
+     * @param _reward The amount of token to be marked as rewarded
      */
-    function reward(address _to, uint256 _value) public onlyAdmin(2) whenNotPaused whenApproved(_to) {
+    function reward(address _to, uint256 _amount, uint256 _reward) public onlyAdmin(2) whenNotPaused whenApproved(_to) {
         uint256 gasLimit = gasleft();
 
         // Add the new reward balance to the recipient
-        super._setRewardBalance(_to, super.safeAdd(super.rewardBalanceOf(_to), _value));
-
+        super._setRewardBalance(_to, super.safeAdd(super.rewardBalanceOf(_to), _reward));
         // Emit a specific event for initialization operation
-        emit Reward(msg.sender, _to, _value);
+        emit Reward(msg.sender, _to, _reward);
 
         // Transfer rewarded token amount
-        require(super._transfer(msg.sender, _to, _value) == true);
-
+        require(super._transfer(msg.sender, _to, _amount) == true);
         // Emit Standard ERC-20 event
-        emit Transfer(msg.sender, _to, _value);
+        emit Transfer(msg.sender, _to, _amount);
 
         // Pay transaction with token instead of ether
         super._payGasInToken(gasLimit);
